187977558e8a38f2f2a6fe2f8392d3479b259da8
Dmitriy Benyuk <dbe@dynamicweb-cms.com>
2024-05-03 14:13:19 +0300

    Support for multiple prices (price matrix) through PriceProvider

--- src/Dynamicweb.Ecommerce.DynamicwebLiveIntegration/TemplateExtenders/ProductViewModelExtensions.cs
+++ src/Dynamicweb.Ecommerce.DynamicwebLiveIntegration/TemplateExtenders/ProductViewModelExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Dynamicweb.Ecommerce.DynamicwebLiveIntegration.Configuration;
 using Dynamicweb.Ecommerce.DynamicwebLiveIntegration.Products;
 using Dynamicweb.Ecommerce.ProductCatalog;
+using Dynamicweb.Ecommerce.Products;
+using Dynamicweb.Security.UserManagement;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,34 +26,40 @@ namespace Dynamicweb.Ecommerce.DynamicwebLiveIntegration
             var user = Helpers.GetCurrentExtranetUser();
             var product = Services.Products.GetProductById(productViewModel.Id, productViewModel.VariantId, productViewModel.LanguageId);
 
+            var prices = GetUnitPrices(settings, user, product);
+            if (prices is not null && prices.Count > 0)
+            {
+                foreach (var price in prices.OrderBy(p => p.Quantity.GetValueOrDefault()))
+                {
+                    result.Add(new()
+                    {
+                        Quantity = price.Quantity ?? 0,
+                        UnitId = price.UnitId,
+                        Price = new()
+                        {
+                            Price = price.Amount.GetValueOrDefault(),
+                            PriceWithVat = price.AmountWithVat.GetValueOrDefault(),
+                            PriceWithoutVat = price.Amount.GetValueOrDefault(),
+                            CurrencyCode = Common.Context.Currency.Code,
+                            ShowPricesWithVat = Common.Context.DisplayPricesWithVat
+                        }
+                    });
+                }
+            }
+            return result;
+        }
+
+        internal static List<ProductPrice> GetUnitPrices(Settings settings, User user, Product product)
+        {
             if (settings is not null && Helpers.CanCheckPrice(settings, product, user))
             {
                 var productInfo = ProductManager.GetProductInfo(product, settings, user);
                 if (productInfo is not null)
-                {                    
-                    var prices = (IList<ProductPrice>)productInfo["Prices"];
-                    if (prices is not null && prices.Count > 0)
-                    {
-                        foreach (var price in prices.OrderBy(p => p.Quantity.GetValueOrDefault()))
-                        {
-                            result.Add(new()
-                            {
-                                Quantity = price.Quantity ?? 0,
-                                UnitId = price.UnitId,
-                                Price = new()
-                                {
-                                    Price = price.Amount.GetValueOrDefault(),
-                                    PriceWithVat = price.AmountWithVat.GetValueOrDefault(),
-                                    PriceWithoutVat = price.Amount.GetValueOrDefault(),
-                                    CurrencyCode = Common.Context.Currency.Code,
-                                    ShowPricesWithVat = Common.Context.DisplayPricesWithVat
-                                }
-                            });
-                        }
-                    }
+                {
+                    return ((IList<ProductPrice>)productInfo["Prices"]).ToList();
                 }
             }
-            return result;
+            return new List<ProductPrice>();
         }
     }
 }
