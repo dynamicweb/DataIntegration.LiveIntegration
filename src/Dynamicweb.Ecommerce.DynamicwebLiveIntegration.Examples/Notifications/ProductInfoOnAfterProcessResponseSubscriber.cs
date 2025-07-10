32287ffa604989211a7d8ed5df899481bc5607ac
Dmitriy Benyuk <dbe@dynamicweb-cms.com>
2025-07-10 13:31:16 +0300

    Post-ProductInfo Processing Hook

---
+++ src/Dynamicweb.Ecommerce.DynamicwebLiveIntegration.Examples/Notifications/ProductInfoOnAfterProcessResponseSubscriber.cs
@@ -0,0 +1,29 @@
+﻿using Dynamicweb.Extensibility.Notifications;
+using Dynamicweb.Ecommerce.DynamicwebLiveIntegration.Notifications;
+
+namespace Dynamicweb.Ecommerce.DynamicwebLiveIntegration.Examples.Notifications
+{
+    /// <summary>
+    /// Class ProductInfoOnAfterProcessResponseSubscriber.
+    /// </summary>
+    /// <seealso cref="NotificationSubscriber" />
+    [Subscribe(ProductInfo.OnAfterGenerateProductInfoXml)]
+    public class ProductInfoOnAfterProcessResponseSubscriber : NotificationSubscriber
+    {
+        /// <summary>
+        /// Call to invoke observer.
+        /// </summary>
+        /// <param name="notification">The notification.</param>
+        /// <param name="args">The args.</param>
+        public override void OnNotify(string notification, NotificationArgs args)
+        {
+            var myArgs = (ProductInfo.OnAfterProductInfoProcessResponseArgs)args;
+
+            // TODO: Add code here
+            if (myArgs?.ProductInfo != null)
+            {
+
+            }
+        }
+    }
+}
